a6dad838fb398fa7088550aeb997208eff8f8d33
Braam Beresford <braam@braam.email>
2020-05-07 18:39:31 -0700

    Issue syncing clocks

--- tbVGA.sv
+++ tbVGA.sv
@@ -9,20 +9,20 @@
 *
 */
 
-`timescale 1ns/1ps
+`timescale 10ns/10ns
 
 module tbVGA;
 
 	//50 MHz (20 ns) master clock
 	parameter M_CYCLE = 20; 
 
-	//25 MHz (20 ns) local clock
-	parameter L_CYCLE = 10; 
+	//25 MHz (40 ns) local clock
+	parameter L_CYCLE = 40; 
 
 
 
 	// Create master clock, active low reset, and local clock
-	logic m_clk, reset_n, l_clk, enable;
+	logic m_clk, reset_n, l_clk;
 
 	// Colour control buses
 	logic blue_control, red_control, green_control;
@@ -32,6 +32,7 @@ module tbVGA;
 
 	logic h_sync, v_sync;
 
+	logic synced;
 
 	// TB internal signals
 	logic reset_tb;
@@ -40,7 +41,7 @@ module tbVGA;
 	vgaDriver DUT(
 			.red(red_control), .green(green_control), .blue(blue_control),
 			.red_(red_display), .green_(green_display), .blue_(blue_display),
-			.h_sync(h_sync), .v_sync(v_sync), .reset(reset_n), .enable(enable),
+			.h_sync(h_sync), .v_sync(v_sync), .reset(reset_n),
 			.clk(m_clk)
 		);
 
@@ -53,59 +54,65 @@ module tbVGA;
 
 	//Create 25MHz clock, common between DUT and TB
 	initial begin
-		l_clk = 1'b1;
+		l_clk = 1'b0;
 		forever #(L_CYCLE/2) l_clk = ~l_clk;
 	end
 
 
 	// Sync to VSYNC intially
 	initial begin
+		synced = 0;
 		blue_control = '0;
 		green_control = '0;
 		red_control = '0;
-
-		enable = 0;			//TODO: Remove
-		reset_n = 0;		//TODO: Remove
-
-		#(L_CYCLE*2);
-
-		enable = 1;			//TODO: Remove
+		reset_tb = 0;
 		reset_n = 1;		//TODO: Remove
 
+		#(L_CYCLE*2);
+		
+
+		reset_n = 0;		//TODO: Remove
+
+	
+
+	
+		//May want to seperate reset and syncing logic
 		//Reset testbench counters
-		@(negedge v_sync); reset_tb = 1;
+		 @(negedge v_sync); reset_tb = 1;
+		synced = 1;
+		reset_n = 1;		//TODO: Remove
 		#(L_CYCLE) reset_tb = 0;
 
 		//Check for blue only
-		blue_control = '1;
-		#(L_CYCLE)
-		assert (green_display == 1'h0 && blue_display == 1'hF && red_display ==1'h0) 
-			else $error("@%d blue failed to assert alone", $time);
-		blue_control = '0;	
+		// blue_control = '1;
+		// #(L_CYCLE)
+		// assert (green_display == 1'h0 && blue_display == 1'hF && red_display ==1'h0) 
+		// 	else $error("@%d blue failed to assert alone", $time);
+		// blue_control = '0;	
 
 		//Check green only
-		green_control = '1;
-		#(L_CYCLE)
-		assert (green_display == 1'hF && blue_display == 1'h0 && red_display ==1'h0 ) 
-				else $error("@%d green failed to assert alone", $time);
-		green_control = '0;
+		// green_control = '1;
+		// #(L_CYCLE)
+		// assert (green_display == 1'hF && blue_display == 1'h0 && red_display ==1'h0 ) 
+		// 		else $error("@%d green failed to assert alone", $time);
+		// green_control = '0;
 
 		//Check red only
-		red_control = '1;
-		#(L_CYCLE)
-		assert (green_display == 1'h0 && blue_display == 1'h0 && red_display ==1'hF ) 
-				else $error("@%d red failed to assert alone", $time);
+		// red_control = '1;
+		// #(L_CYCLE)
+		// assert (green_display == 1'h0 && blue_display == 1'h0 && red_display ==1'hF ) 
+		// 		else $error("@%d red failed to assert alone", $time);
 		
 		//Check all three colors together
 		blue_control = '1;
 		green_control = '1;
-		#(L_CYCLE)
-		assert (green_display == 1'h0 && blue_display == 1'h0 && red_display ==1'hF ) 
-				else $error("@%d not all colours asserted simultaneously", $time);
+		// #(L_CYCLE)
+		// assert (green_display == 1'h0 && blue_display == 1'h0 && red_display ==1'hF ) 
+		// 		else $error("@%d not all colours asserted simultaneously", $time);
 
-		//Done!
-		$stop;
-
+		// //Done!
+		
+	 
 	end
 
 	//Count clock cycles of both HSYNC and VSYN
@@ -121,78 +128,82 @@ module tbVGA;
 		//Reached end of row
 		if(h_clk_count >= 800) begin
 			//Check for end of screen
-			if(v_clk_count <525)
+			if(v_clk_count <524)
 				v_clk_count = v_clk_count +1;
 			else 
 				v_clk_count = 0;
 
 			h_clk_count = 0;
 		end
+		
 	end
 
 
 	// Check VSYNC
-	always @(v_clk_count) begin
+	always @ (posedge l_clk) begin
 		//Always assert low color during SYNC period
-		assert (!(!v_sync && (blue_display || red_display) || green_display))
-			else $error("@%d a colour was active during the VSYNC sync period. ", $time);
+		assert (!(!v_sync && (blue_display || red_display || green_display)))
+			else $error("@%d a colour was active during the VSYNC sync period. ", v_clk_count);
 
 		
 		if(v_clk_count < 2) 
-			assert (v_sync == 0) else $error("@%d VSYNC was not LOW during sync period", $time);
+			assert (v_sync == 0) else $error("@%d VSYNC was not LOW during sync period", v_clk_count);
 		
 		else if(v_clk_count < 35)begin
-			assert(v_sync == 1) else $error("@%d VSYNC was LOW during front porch", $time);
+			assert(v_sync == 1) else $error("@%d VSYNC was LOW during back porch", v_clk_count);
 
 			assert(blue_display == red_display == green_display == 0) 
-				else $error("@%d a colour is non-zero during back porch", $time);
+				else $error("@%d a colour is non-zero during back porch", v_clk_count);
 		end
 
 
 		else if(v_clk_count < 515)
-			assert(v_sync == 1) else $error("@%d VSYNC was LOW during display", $time);
+			assert(v_sync == 1) else $error("@%d VSYNC was LOW during display", v_clk_count);
 
 		else if(v_clk_count < 525) begin
-			assert(v_sync == 1) else $error("@%d VSYNC was LOW during front porch", $time);
+			assert(v_sync == 1) else $error("@%d VSYNC was LOW during front porch", v_clk_count);
 			assert(blue_display == red_display == green_display == 0) 
-				else $error("@%d a colour is non-zero during front porch", $time);
+				else $error("@%d a colour is non-zero during front porch", v_clk_count);
 		end
 
 		else
 			$error("Testbench v_clk_count too large");
 	end
-
+	
 
 	// Check HSYNC
-	always @(h_clk_count) begin
-		//Always assert low color during SYNC period
-		assert (!(!h_sync && (blue_display || red_display) || green_display)) 
-			else $display("@%d a colour was active during the VSYNC sync period. ", $time);
+	// always @(h_clk_count) begin
+	// if(synced) begin
+	// 	//Always assert low color during SYNC period
+	// 	assert ((!h_sync && (blue_display || red_display) || green_display)) 
+	// 		else $display("@%d a colour was active during the VSYNC sync period. ", $time);
 
 
-		if(h_clk_count < 96) 
-			assert (h_sync == 0) else $display("@%d HSYNC was not LOW during sync period", $time);
+	// 	if(h_clk_count < 96) 
+	// 		assert (h_sync == 0) else $display("@%d HSYNC was not LOW during sync period", $time);
 		
-		else if(h_clk_count < 144)begin
-			assert(h_sync == 1) else $display("@%d HSYNC was LOW during front porch", $time);
+	// 	else if(h_clk_count < 144)begin
+	// 		assert(h_sync == 1) else $display("@%d HSYNC was LOW during front porch", $time);
 
-			assert(blue_display == red_display == green_display == 0) 
-				else $display("@%d a colour is non-zero during back porch", $time);
-		end
+	// 		assert(blue_display == red_display == green_display == 0) 
+	// 			else $display("@%d a colour is non-zero during back porch", $time);
+	// 	end
 
 
-		else if(h_clk_count < 784)
-			assert(h_sync == 1) else $display("@%d HSYNC was LOW during display", $time);
+	// 	else if(h_clk_count < 784)
+	// 		assert(h_sync == 1) else $display("@%d HSYNC was LOW during display", $time);
 
-		else if(h_clk_count < 800) begin
-			assert(h_sync == 1) else $display("@%d HSYNC was LOW during front porch", $time);
-			assert(blue_display == red_display == green_display == 0) 
-				else $display("@%d a colour is non-zero during front porch", $time);
-		end
+	// 	else if(h_clk_count < 800) begin
+	// 		assert(h_sync == 1) else $display("@%d HSYNC was LOW during front porch", $time);
+	// 		assert(blue_display == red_display == green_display == 0) 
+	// 			else $display("@%d a colour is non-zero during front porch", $time);
+	// 	end
 
-		else
-			$error("Testbench h_clk_count too large");
-	end
+	// 	else
+	// 		$error("Testbench h_clk_count too large");
+
+	// 	end
+	// end
 
 
 endmodule 
