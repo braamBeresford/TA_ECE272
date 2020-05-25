2a0510fbe245a0faf4df400d2367b6cc67be5164
Braam Beresford <braam@braam.email>
2020-05-24 17:49:53 -0700

    Working prototype

--- tbVGA.sv
+++ tbVGA.sv
@@ -19,8 +19,6 @@ module tbVGA;
 	//25 MHz (40 ns) local clock
 	parameter L_CYCLE = 40; 
 
-
-
 	// Create master clock, active low reset, and local clock
 	logic m_clk, reset_n, l_clk;
 
@@ -66,21 +64,19 @@ module tbVGA;
 	initial begin
 		in_h_display = 0;
 		in_v_display = 0;
+
 		synced = 0;
+
 		blue_control = '0;
 		green_control = '0;
 		red_control = '0;
-		reset_tb = 0;
-		reset_n = 1;		//TODO: Remove
 
+		reset_tb = 0;
+		reset_n = 1;		
 
 		#(L_CYCLE*2);
 		
-		
-
-		reset_n = 0;		//TODO: Remove
-
-	
+		reset_n = 0;		
 
 		#(L_CYCLE*2)
 		reset_n =1;
@@ -88,14 +84,14 @@ module tbVGA;
 
 		//May want to seperate reset and syncing logic
 		//Reset testbench counters
+		//Sync on posedge coming out of the v-sync period
 		@(posedge v_sync); 
 		reset_tb = 1;
 		#(L_CYCLE) 
 		reset_tb = 0;
 
-		$display("HELLO");
 		synced = 1;
-		// reset_n = 1;		//TODO: Remove
+
 		#(L_CYCLE/2) reset_tb = 0;
 
 		//Check for blue only
@@ -118,36 +114,26 @@ module tbVGA;
 		blue_control = '1;
 		green_control = '1;
 		#100ms
-		// assert (green_display == 1'h0 && blue_display == 1'h0 && red_display ==1'hF ) 
-		// 		else $error("@%d not all colours asserted simultaneously", $time);
 
 		//Done!
-		$display("Test bench passes");
+		$display("Test bench passes! Good job!");
 		$finish;
 	 
 	end
 
 
-		always @(h_clk_count) begin
+	//Check that colours are being asserted when requested
+	always @(h_clk_count) begin
 		#(L_CYCLE/9)
 		if( in_v_display && in_h_display) begin
-					assert(!(blue_control^ blue_display == 4'b1111))
+					assert(!(blue_control ^ blue_display == 4'b1111))
 						 else $error("@%d Blue wasn't active when blue_control asserted", $time);
-					assert(!(red_control^ red_display == 4'b1111))
+					assert(!(red_control ^ red_display == 4'b1111))
 						 else $error("@%d Red wasn't active when blue_control asserted", $time);
-					assert(!(green_control^ green_display == 4'b1111))
+					assert(!(green_control ^ green_display == 4'b1111))
 						 else $error("@%d Green wasn't active when blue_control asserted", $time);
 		end
 	end
-
-	// always @(h_clk_count) begin
-	// 	#(L_CYCLE/9)
-	// 	if(blue_control && h_clk_count <= 784 && h_clk_count > 144) begin
-	// 				if(v_clk_count >= 35 && v_clk_count < 515)
-	// 				assert(blue_display == 4'b1111)
-	// 					 else $error("@%d Blue wasn't active when blue_control asserted", $time);
-	// 	end
-	// end
 
 	//Count clock cycles of both HSYNC and VSYN
 	int v_clk_count;
@@ -175,17 +161,15 @@ module tbVGA;
 
 	//Check VSYNC
 	always @ (v_clk_count) begin
+
 		//Cause delay as to not sample on the clock face, 
 		//I think there must be a cleaner way of doing this (maybe clocking)
 		#(L_CYCLE/15)
-		//Always assert low color during SYNC period
-		// assert (!(!v_sync && (blue_display || red_display || green_display)))
-		// 	else $error("@%d a colour was active during the VSYNC sync period. ", v_clk_count);
-
 		
+		//Always assert low color during SYNC period
 		if(v_clk_count < 2) begin
 			in_v_display = 0;
-			assert (v_sync == 0) else $error("@%d VSYNC was not LOW during sync period", v_clk_count);
+			assert (v_sync == 0) else $error("@%t VSYNC was not LOW during sync period", v_clk_count);
 		end
 
 		else if(v_clk_count < 35)begin
@@ -210,7 +194,7 @@ module tbVGA;
 		end
 
 		else
-			$error("Testbench v_clk_count too large");
+			$error("Testbench v_clk_count too large, report this error to TAs");
 	end
 	
 
@@ -219,11 +203,6 @@ module tbVGA;
 		//Cause delay as to not sample on the clock face
 		#(L_CYCLE/15)
 		if(synced) begin
-			//Always assert low color during SYNC period
-			// assert (!(!h_sync && (blue_display || red_display) || green_display)) 
-			// 	else $display("@%d a colour was active during the VSYNC sync period. ", $time);
-
-
 			if(h_clk_count <= 96) begin
 				in_h_display = 0;
 				assert (h_sync == 0) else $error("@%d HSYNC was not LOW during sync period", $time);
@@ -250,10 +229,8 @@ module tbVGA;
 			end
 
 			else
-				$error("Testbench h_clk_count too large");
+				$error("Testbench h_clk_count too large, report this error to TAs");
 
 			end
 	end
-
-
 endmodule 
